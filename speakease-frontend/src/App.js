1bf0e9c2443ad026f97bddb5c28d91f1c3a7482f
OrelShai <orelshai991010@gmail.com>
2025-03-07 01:06:25 +0200

    init home page

--- speakease-frontend/src/App.js
+++ speakease-frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import EditProfile from './pages/EditProfile';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/edit-profile" element={<EditProfile />} />
+          <Route path="/homepage" element={<HomePage />} />
         </Routes>
       </div>
     </Router>
